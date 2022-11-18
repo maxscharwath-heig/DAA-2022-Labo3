6d48696b6251e209701c0c688464830180c77c18
Lazzzer <dev@lazarpavicevic.ch>
2022-11-18 09:10:51 +0100

    chore: add fragments and view model

---
+++ app/src/main/java/ch/heigvd/daa_labo3/fragments/NotesFragment.kt
@@ -0,0 +1,32 @@
+package ch.heigvd.daa_labo3.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ch.heigvd.daa_labo3.R
+import ch.heigvd.daa_labo3.viewmodels.NotesViewModel
+
+class NotesFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = NotesFragment()
+    }
+
+    private lateinit var viewModel: NotesViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_notes, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(NotesViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+}
---
+++ app/src/main/java/ch/heigvd/daa_labo3/viewmodels/NotesViewModel.kt
@@ -0,0 +1,7 @@
+package ch.heigvd.daa_labo3.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class NotesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
