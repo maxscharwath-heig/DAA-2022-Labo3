afd4f19b3ed7753a1191fb2001089e656519b078
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-11-18 10:01:39 +0100

    MenuBar done, trying to do a tablet layout

--- app/src/main/java/ch/heigvd/daa_labo3/MainActivity.kt
+++ app/src/main/java/ch/heigvd/daa_labo3/MainActivity.kt
@@ -2,10 +2,39 @@ package ch.heigvd.daa_labo3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.creation_date_filter -> {
+                /* TODO: filter by creation date */
+                true
+            }
+            R.id.eta_filter -> {
+                /* TODO: filter by eta */
+                true
+            }
+            R.id.menu_actions_generate -> {
+                /* TODO: generate data */
+                true
+            }
+            R.id.menu_actions_delete_all -> {
+                /* TODO: delete all */
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
