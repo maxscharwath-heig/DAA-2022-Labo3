b4c878aaf8b4fd0a87cab6b4b26deb004b3b5ddb
Lazzzer <dev@lazarpavicevic.ch>
2022-11-25 09:29:54 +0100

    feat: structure of recycleView is better

--- app/src/main/java/ch/heigvd/daa_labo3/fragments/ControlsRecyclerAdapter.kt
+++ app/src/main/java/ch/heigvd/daa_labo3/fragments/ControlsRecyclerAdapter.kt
@@ -3,18 +3,33 @@ package ch.heigvd.daa_labo3.fragments
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import ch.heigvd.daa_labo3.R
 import ch.heigvd.daa_labo3.models.Note
+import ch.heigvd.daa_labo3.models.State.*
 import ch.heigvd.daa_labo3.models.Type.*
 
 
 class ControlsRecyclerAdapter(_items : List<Note> = listOf()) : RecyclerView.Adapter<ControlsRecyclerAdapter.ViewHolder>() {
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val iconNote = view.findViewById<ImageView>(R.id.list_item_icon)
         private val titleNote = view.findViewById<TextView>(R.id.list_item_title)
         private val textNote = view.findViewById<TextView>(R.id.list_item_text)
         fun bind(note : Note) {
+            iconNote.setImageResource(when(note.type) {
+                TODO -> R.drawable.todo
+                SHOPPING -> R.drawable.shopping
+                WORK -> R.drawable.work
+                FAMILY -> R.drawable.family
+                else -> R.drawable.note
+            })
+            iconNote.imageTintList = when(note.state) {
+                IN_PROGRESS -> ContextCompat.getColorStateList(iconNote.context, R.color.grey)
+                DONE -> ContextCompat.getColorStateList(iconNote.context, R.color.green)
+            }
             titleNote?.text = note.title
             textNote?.text = note.text
         }
@@ -35,13 +50,7 @@ class ControlsRecyclerAdapter(_items : List<Note> = listOf()) : RecyclerView.Ada
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return when(viewType) {
-            TODO.ordinal -> ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_notes, parent, false))
-            SHOPPING.ordinal -> ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_notes, parent, false))
-            WORK.ordinal -> ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_notes, parent, false))
-            FAMILY.ordinal -> ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_notes, parent, false))
-            else -> /* NONE */ ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_notes, parent, false))
-        }
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_notes, parent, false))
     }
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(items[position])
