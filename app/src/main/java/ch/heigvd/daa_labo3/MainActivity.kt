ba4920939619d4c8a88a21b0a0f9a1d71d0794df
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-11-28 14:45:35 +0100

    View model crashing

--- app/src/main/java/ch/heigvd/daa_labo3/MainActivity.kt
+++ app/src/main/java/ch/heigvd/daa_labo3/MainActivity.kt
@@ -4,17 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
+import ch.heigvd.daa_labo3.viewmodels.NotesViewModel
+import ch.heigvd.daa_labo3.viewmodels.NotesViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: NotesViewModel by viewModels<NotesViewModel> {
+        NotesViewModelFactory((application as MyApp).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+/*
         println("Chargement de l'activité principale")
         println((application as MyApp).repository.allNotes.value)
         (application as MyApp).repository.allNotes.observe(this) { notes ->
             println("Notes reçues: $notes")
         }
+
+ */
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -25,19 +34,19 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.creation_date_filter -> {
-                /* TODO: filter by creation date */
+                viewModel.sortByCreationDate()
                 true
             }
             R.id.eta_filter -> {
-                /* TODO: filter by eta */
+                viewModel.sortByETA()
                 true
             }
             R.id.menu_actions_generate -> {
-                /* TODO: generate data */
+                viewModel.generateANote()
                 true
             }
             R.id.menu_actions_delete_all -> {
-                /* TODO: delete all */
+                viewModel.deleteAllNote()
                 true
             }
             else -> super.onOptionsItemSelected(item)
--- app/src/main/java/ch/heigvd/daa_labo3/viewmodels/NotesViewModel.kt
+++ app/src/main/java/ch/heigvd/daa_labo3/viewmodels/NotesViewModel.kt
@@ -1,8 +1,38 @@
 package ch.heigvd.daa_labo3.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ch.heigvd.daa_labo3.repositories.DataRepository
 
-class NotesViewModel : ViewModel() {
+class NotesViewModel (private val repository: DataRepository) : ViewModel() {
+
+    val allNotes = repository.allNotes
+
+    val countNotes = repository.countNotes
+
     // TODO: Implement the ViewModel
+    fun sortByCreationDate() {
 
+    }
+
+    fun sortByETA() {
+
+    }
+
+    fun generateANote() {
+        repository.generateANote()
+    }
+
+    fun deleteAllNote() {
+        repository.deleteAllNotes()
+    }
+}
+
+class NotesViewModelFactory(private val repository: DataRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(NotesViewModel::class.java)) {
+            return NotesViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
