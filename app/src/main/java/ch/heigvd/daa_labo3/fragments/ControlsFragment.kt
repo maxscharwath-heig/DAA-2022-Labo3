c3578d5f9fdf7c46ac95b5d0a20f0dc922fb0083
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-12-01 11:14:42 +0100

    ControlFragment

--- app/src/main/java/ch/heigvd/daa_labo3/fragments/ControlsFragment.kt
+++ app/src/main/java/ch/heigvd/daa_labo3/fragments/ControlsFragment.kt
@@ -1,21 +1,21 @@
 package ch.heigvd.daa_labo3.fragments
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import ch.heigvd.daa_labo3.MyApp
 import ch.heigvd.daa_labo3.R
 import ch.heigvd.daa_labo3.viewmodels.NotesViewModel
+import ch.heigvd.daa_labo3.viewmodels.NotesViewModelFactory
 
 class ControlsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ControlsFragment()
+    private val viewModel: NotesViewModel by activityViewModels {
+        NotesViewModelFactory((requireActivity().application as MyApp).repository)
     }
-
-    private lateinit var viewModel: NotesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,7 +26,5 @@ class ControlsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // viewModel = ViewModelProvider(this)[NotesViewModel::class.java]
-        // TODO: Use the ViewModel
     }
 }
