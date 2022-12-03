4d0cd319e0c48a5cd176333f2477c61ea5abab7a
Lazzzer <dev@lazarpavicevic.ch>
2022-12-03 18:30:08 +0100

    fix: eta filter is correctly applied

--- app/src/main/java/ch/heigvd/daa_labo3/fragments/NotesRecyclerAdapter.kt
+++ app/src/main/java/ch/heigvd/daa_labo3/fragments/NotesRecyclerAdapter.kt
@@ -25,6 +25,30 @@ import java.util.concurrent.TimeUnit
  */
 class NotesRecyclerAdapter(_items: List<NoteAndSchedule> = listOf()) :
     RecyclerView.Adapter<NotesRecyclerAdapter.ViewHolder>() {
+
+    var items = listOf<NoteAndSchedule>()
+        set(value) {
+            val diffCallback = NotesDiffCallback(items, value)
+            val diffItems = DiffUtil.calculateDiff(diffCallback)
+            field = value
+            diffItems.dispatchUpdatesTo(this)
+        }
+
+    init {
+        items = _items
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun getItemViewType(position: Int): Int {
+        return if (items[position].schedule != null) NOTE_SCHEDULE else NOTE
+    }
+
+    companion object {
+        private const val NOTE = 0
+        private const val NOTE_SCHEDULE = 1
+    }
+
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val iconNote = view.findViewById<ImageView>(R.id.list_item_icon)
         private val titleNote = view.findViewById<TextView>(R.id.list_item_title)
@@ -49,10 +73,8 @@ class NotesRecyclerAdapter(_items: List<NoteAndSchedule> = listOf()) :
             }
             titleNote?.text = noteAndSchedule.note.title
             textNote?.text = noteAndSchedule.note.text
-            if (noteAndSchedule.schedule != null) {
-                iconSchedule.visibility = View.VISIBLE
-                textSchedule.visibility = View.VISIBLE
 
+            if (noteAndSchedule.schedule != null) {
                 val (dateText, isLate) = displayDateDifference(
                     itemView.context,
                     noteAndSchedule.schedule.date
@@ -65,92 +87,79 @@ class NotesRecyclerAdapter(_items: List<NoteAndSchedule> = listOf()) :
                 }
             }
         }
-    }
 
-    var items = listOf<NoteAndSchedule>()
-        set(value) {
-            val diffCallback = NotesDiffCallback(items, value)
-            val diffItems = DiffUtil.calculateDiff(diffCallback)
-            field = value
-            diffItems.dispatchUpdatesTo(this)
+        private fun displayDateDifference(context: Context, dueDate: Calendar): Pair<String, Boolean> {
+            val today = Calendar.getInstance()
+            val diff = dueDate.timeInMillis.minus(today.timeInMillis)
+
+            if (diff <= 0) return Pair(context.getString(R.string.schedule_late), true)
+
+            val diffMinutes = TimeUnit.MINUTES.convert(diff, TimeUnit.MILLISECONDS)
+            val diffHours = TimeUnit.HOURS.convert(diff, TimeUnit.MILLISECONDS)
+            val diffDays = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS)
+            val diffWeeks = diffDays.div(today.getActualMaximum(Calendar.DAY_OF_WEEK))
+            val diffMonths = diffDays.div(today.getActualMaximum(Calendar.DAY_OF_MONTH))
+
+            when {
+                diffMonths > 0 ->
+                    return Pair(
+                        context.resources.getQuantityString(
+                            R.plurals.schedule_month,
+                            diffMonths.toInt(),
+                            diffMonths
+                        ), false
+                    )
+                diffWeeks > 0 ->
+                    return Pair(
+                        context.resources.getQuantityString(
+                            R.plurals.schedule_week,
+                            diffWeeks.toInt(),
+                            diffWeeks
+                        ), false
+                    )
+                diffDays > 0 ->
+                    return Pair(
+                        context.resources.getQuantityString(
+                            R.plurals.schedule_day,
+                            diffDays.toInt(),
+                            diffDays
+                        ), false
+                    )
+                diffHours > 0 ->
+                    return Pair(
+                        context.resources.getQuantityString(
+                            R.plurals.schedule_hour,
+                            diffHours.toInt(),
+                            diffHours
+                        ), false
+                    )
+                diffMinutes > 0 ->
+                    return Pair(
+                        context.resources.getQuantityString(
+                            R.plurals.schedule_minute,
+                            diffMinutes.toInt(),
+                            diffMinutes
+                        ), false
+                    )
+                else -> return Pair(
+                    context.getString(R.string.schedule_late),
+                    true
+                ) // We assume if the remaining time is less than a minute, it's late
+            }
         }
-
-    init {
-        items = _items
-    }
-
-    private fun displayDateDifference(
-        context: Context,
-        dueDate: Calendar
-    ): Pair<String, Boolean> {
-        val diff = dueDate.timeInMillis.minus(Calendar.getInstance().timeInMillis)
-
-        if (diff <= 0) return Pair(context.getString(R.string.schedule_late), true)
-
-        val diffMinutes = TimeUnit.MINUTES.convert(diff, TimeUnit.MILLISECONDS)
-        val diffHours = TimeUnit.HOURS.convert(diff, TimeUnit.MILLISECONDS)
-        val diffDays = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS)
-        val diffWeeks = diffDays.div(dueDate.getActualMaximum(Calendar.DAY_OF_WEEK))
-        val diffMonths = diffDays.div(dueDate.getActualMaximum(Calendar.DAY_OF_MONTH))
-
-        when {
-            diffMonths > 0 ->
-                return Pair(
-                    context.resources.getQuantityString(
-                        R.plurals.schedule_month,
-                        diffMonths.toInt(),
-                        diffMonths
-                    ), false
-                )
-            diffWeeks > 0 ->
-                return Pair(
-                    context.resources.getQuantityString(
-                        R.plurals.schedule_week,
-                        diffWeeks.toInt(),
-                        diffWeeks
-                    ), false
-                )
-            diffDays > 0 ->
-                return Pair(
-                    context.resources.getQuantityString(
-                        R.plurals.schedule_day,
-                        diffDays.toInt(),
-                        diffDays
-                    ), false
-                )
-            diffHours > 0 ->
-                return Pair(
-                    context.resources.getQuantityString(
-                        R.plurals.schedule_hour,
-                        diffHours.toInt(),
-                        diffHours
-                    ), false
-                )
-            diffMinutes > 0 ->
-                return Pair(
-                    context.resources.getQuantityString(
-                        R.plurals.schedule_minute,
-                        diffMinutes.toInt(),
-                        diffMinutes
-                    ), false
-                )
-            else -> return Pair(
-                context.getString(R.string.schedule_late),
-                true
-            ) // We assume if the remaining time is less than a minute, it's late
-        }
-    }
-
-    override fun getItemCount() = items.size
-
-    override fun getItemViewType(position: Int): Int {
-        return items[position].note.type.ordinal
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.list_item_notes, parent, false)
-        )
+        return when (viewType) {
+            NOTE -> ViewHolder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.list_item_note, parent, false)
+            )
+            else -> ViewHolder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.list_item_note_schedule, parent, false)
+            )
+        }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
